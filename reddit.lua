ad04f45d4feecde30ad1ef38c2bba568bac86f93
arkiver <arkiver@protonmail.com>
2021-03-30 16:11:12 +0200

    Fix typo.

--- reddit.lua
+++ reddit.lua
@@ -486,7 +486,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
           abort_item()
         end
         if selftext then
-          io.stdout:write("sefltext already found.\n")
+          io.stdout:write("selftext already found.\n")
           io.stdout:flush()
           abort_item()
         end
