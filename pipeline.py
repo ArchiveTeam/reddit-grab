e6b876e9e64d04b1a5391beec37257c46347cb89
km09 <2054623+km09@users.noreply.github.com>
2021-05-06 00:07:16 +0100

    New day.. new wget-at 1.20.3-at.20210504.01

--- pipeline.py
+++ pipeline.py
@@ -43,7 +43,8 @@ WGET_AT = find_executable(
     'Wget+AT',
     [
 		'GNU Wget 1.20.3-at.20210410.01',
-		'GNU Wget 1.20.3-at.20210212.02'
+		'GNU Wget 1.20.3-at.20210212.02',
+	    	'GNU Wget 1.20.3-at.20210504.01'
 	],
     [
          './wget-at',
