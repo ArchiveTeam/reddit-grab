0c5e8cd3bdbb70e105cec07bf740fed8ffc889a0
arkiver <arkiver@protonmail.com>
2021-10-01 02:44:01 +0200

    Version 20211001.01. Use GNU Wget 1.20.3-at.20211001.01.

--- pipeline.py
+++ pipeline.py
@@ -42,7 +42,7 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 WGET_AT = find_executable(
     'Wget+AT',
     [
-		'GNU Wget 1.20.3-at.20210504.01'
+		'GNU Wget 1.20.3-at.20211001.01'
 	],
     [
          './wget-at',
@@ -59,7 +59,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210707.01'
+VERSION = '20211001.01'
 TRACKER_ID = 'reddit'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 20
