4f50a0d6995e9cd65b80e22ef717cecbda4c5f16
arkiver <arkiver@protonmail.com>
2022-03-11 03:52:49 +0100

    Version 20220311.01. Use new backfeed endpoint for queuing.

--- reddit.lua
+++ reddit.lua
@@ -666,8 +666,8 @@ wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total
     local tries = 0
     while tries < 10 do
       local body, code, headers, status = http.request(
-        "http://blackbird-amqp.meo.ws:23038/urls-t05crln9brluand/",
-        items
+        "https://legacy-api.arpa.li/backfeed/legacy/urls-dyr8rrotminvxky",
+        items .. "\0"
       )
       if code == 200 or code == 409 then
         break
