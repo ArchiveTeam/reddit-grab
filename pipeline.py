2d8fa4034b81918c233b54376f0893dac4460963
arkiver <arkiver@protonmail.com>
2022-07-28 18:15:54 +0200

    Version 20220729.02. Support older Wget versions.

--- pipeline.py
+++ pipeline.py
@@ -42,6 +42,8 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 WGET_AT = find_executable(
     'Wget+AT',
     [
+        'GNU Wget 1.20.3-at.20211001.01',
+        'GNU Wget 1.21.3-at.20220503.02',
         'GNU Wget 1.21.3-at.20220608.02'
     ],
     [
@@ -59,7 +61,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20220729.01'
+VERSION = '20220729.02'
 TRACKER_ID = 'reddit'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 20
