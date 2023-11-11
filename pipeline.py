8fc86a11ca8d34ec4d00b1bfa6e23178b0bbe2ac
arkiver <arkiver@protonmail.com>
2023-11-11 18:24:22 +0100

    Version 20231111.02.

--- pipeline.py
+++ pipeline.py
@@ -72,7 +72,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20231111.01'
+VERSION = '20231111.02'
 TRACKER_ID = 'reddit'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 100
